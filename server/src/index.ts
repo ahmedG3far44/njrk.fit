b3ff486bc90058e9b27233748af0dc97f0a9fd53
ahmedG3far44 <ahmedjaafarbadri@gmail.com>
2026-06-25 23:50:17 +0300

    style: standardize import formatting and improve code indentation in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,23 @@
-import app from './app';
-import dbConnection from './configs/db';
+import app from "./app";
+import dbConnection from "./configs/db";
 
-import { env } from './configs/env';
-import { seedAdmins } from './seeders/seedAdmin';
+import { env } from "./configs/env";
+import { seedAdmins } from "./seeders/seedAdmin";
 
 async function startServer() {
-    await dbConnection;
+  await dbConnection;
 
-    try {
-        await seedAdmins();
-    } catch (err) {
-        console.error('Admin seeding failed:', err);
-    }
+  try {
+    await seedAdmins();
+  } catch (err) {
+    console.error("Admin seeding failed:", err);
+  }
 
-    app.listen(env.PORT, '0.0.0.0', () => {
-        console.log(`Server is running on http://localhost:${env.PORT} in ${env.NODE_ENV} mode`);
-    });
+  app.listen(env.PORT, "0.0.0.0", () => {
+    console.log(
+      `Server is running on ${env.NODE_ENV === "production" ? "https://api.njerka.xyz" : `http://localhost:${env.PORT}`} in ${env.NODE_ENV} mode`,
+    );
+  });
 }
-
 
 startServer();
